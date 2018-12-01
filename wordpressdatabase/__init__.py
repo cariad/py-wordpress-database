196357423bd079bc95c81dd88225073966d947bb
Cariad Eccleston <cariad@cariad.me>
2018-12-01 17:09:41 +0000

    WIP

--- wordpressdatabase/__init__.py
+++ wordpressdatabase/__init__.py
@@ -77,6 +77,9 @@ def ensure(engine,
     # Database names cannot be parameterized, so validate it and be careful.
     cursor.execute('CREATE DATABASE IF NOT EXISTS {};'.format(db_name))
 
+    logger.info('Using database "%s"...', db_name)
+    cursor.execute('USE {};'.format(db_name))
+
     logger.info('Ensuring user "%s" exists...', wp_username)
     cursor.execute('GRANT ALL PRIVILEGES ON {} TO %s IDENTIFIED BY %s;'.format(db_name),
                    (wp_username, wp_password))
