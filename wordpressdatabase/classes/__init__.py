85bb16bb4f38ec1ef693fb4bbaba5b8461e789b4
Cariad Eccleston <cariad@cariad.me>
2018-12-01 20:06:07 +0000

    WIP

--- wordpressdatabase/classes/__init__.py
+++ wordpressdatabase/classes/__init__.py
@@ -3,7 +3,6 @@ Classes.
 """
 
 from wordpressdatabase.classes.database import Database
-from wordpressdatabase.classes.database_identifier import DatabaseIdentifier
 # The Credentials module will import Secret, so we need to import Secret here
 # first.
 from wordpressdatabase.classes.secret import Secret
