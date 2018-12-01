24054c62bfba5ea64ad4af8045518df198464de5
Cariad Eccleston <cariad@cariad.me>
2018-12-01 12:20:11 +0000

    Fix import order

--- wordpressdatabase/classes/__init__.py
+++ wordpressdatabase/classes/__init__.py
@@ -2,5 +2,7 @@
 Classes.
 """
 
-from wordpressdatabase.classes.credentials import Credentials
+# The Credentials module will import Secret, so we need to import Secret here
+# first.
 from wordpressdatabase.classes.secret import Secret
+from wordpressdatabase.classes.credentials import Credentials
