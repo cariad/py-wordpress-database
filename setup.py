0726e4419c7dc0a41f386d31b0778af2d41a3a32
Cariad Eccleston <cariad@cariad.me>
2018-12-04 20:37:18 +0000

    Clean up

--- setup.py
+++ setup.py
@@ -1,31 +1,45 @@
 """
-"wordpress-database" package setup.
+wpdatabase package setup.
 """
 
 from setuptools import setup
 
+with open('README.md', 'r') as stream:
+    LONG_DESCRIPTION = stream.read()
+
 setup(
-    name='wordpressdatabase',
-    version='0.1',
-    description='Create and interact with a WordPress database.',
-    url='https://github.com/cariad/py-wordpress-database',
     author='Cariad Eccleston',
     author_email='cariad@cariad.me',
-    license='MIT',
-    packages=[
-        'wordpressdatabase',
-        'wordpressdatabase.classes',
-        'wordpressdatabase.exceptions'
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Site Management'
     ],
+    description='A Python package to initialize a WordPress database.',
+    extras_require={
+        'dev': [
+            'autopep8',
+            'coverage',
+            'pylint'
+        ]
+    },
     install_requires=[
         'boto3~=1.9.0',
         'mysql-connector~=2.1.0',
         'wpconfigr~=1.0.0'
     ],
-    extras_require={
-        'dev': [
-            'autopep8',
-            'pylint'
-        ]
-    }
-)
+    name='wpdatabase',
+    license='MIT',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=[
+        'wpdatabase',
+        'wpdatabase.exceptions'
+    ],
+    url='https://github.com/cariad/py-wordpress-database',
+    version='1.0')
