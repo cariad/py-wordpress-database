9f28441b75705a718a65037ddce09f60225e4a70
Cariad Eccleston <cariad@cariad.me>
2018-12-04 20:40:21 +0000

    Add classes

---
+++ wpdatabase/classes/__init__.py
@@ -0,0 +1,9 @@
+"""
+wpdatabase classes.
+"""
+
+from wpdatabase.classes.database import Database
+
+# Credentials will import Secret, so must import Secret here first.
+from wpdatabase.classes.secret import Secret
+from wpdatabase.classes.credentials import Credentials
